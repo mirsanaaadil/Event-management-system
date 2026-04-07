17eded5cdf4f8b1b5f507a040e56c37b144e8868
mirsanaaadil <mirsanalatheef.canfig@gmail.com>
2026-04-07 11:02:50 +0530

    Initial commit - Event Management System

---
+++ db.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+conn = sqlite3.connect("database.db")
+cur = conn.cursor()
+
+
+cur.execute("CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY, username TEXT, password TEXT, role TEXT)")
+cur.execute("CREATE TABLE IF NOT EXISTS events(id INTEGER PRIMARY KEY, name TEXT, date TEXT, location TEXT, description TEXT)")
+cur.execute("CREATE TABLE IF NOT EXISTS registrations(id INTEGER PRIMARY KEY, user_id INTEGER, event_id INTEGER)")
+
+
+cur.execute("INSERT OR IGNORE INTO users VALUES (1, 'admin', 'admin123', 'admin')")
+cur.execute("INSERT OR IGNORE INTO users VALUES (2, 'user1', 'user123', 'user')")
+
+conn.commit()
+conn.close()
+
+print("database created successfully")
